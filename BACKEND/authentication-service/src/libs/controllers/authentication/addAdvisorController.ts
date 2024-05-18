1a8a72a423acf5ca75c2a69ef58f75024e6a498a
officialrashid <muhammedrashi59@gmail.com>
2024-05-18 19:05:33 +0530

    CICD test in authentication service

--- BACKEND/authentication-service/src/libs/controllers/authentication/addAdvisorController.ts
+++ BACKEND/authentication-service/src/libs/controllers/authentication/addAdvisorController.ts
@@ -8,6 +8,9 @@ export default (dependencies:any)=>{
         useCase: {addAdvisor_Usecase}
     } = dependencies
  const addAdvisorController = async (req:Request,res:Response)=>{
+
+
+    
     const response = await addAdvisor_Usecase(dependencies).executeFunction(req.body)
     res.status(201).json(response)
     
