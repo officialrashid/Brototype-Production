d5d7e4e5a80750e9fdce69f7c735649c4ca90b96
officialrashid <muhammedrashi59@gmail.com>
2024-05-18 19:14:07 +0530

    CICD test

--- BACKEND/authentication-service/src/libs/controllers/authentication/advisorGoogleLoginController.ts
+++ BACKEND/authentication-service/src/libs/controllers/authentication/advisorGoogleLoginController.ts
@@ -10,7 +10,7 @@ export default (dependencies:any)=>{
  const advisorGoogleLoginController = async (req:Request,res:Response)=>{
     console.log(req.body,"email boduyyy yarrr");
     const {email} = req.body
-    console.log(email,"email coming yarrr");
+   
     
     const response = await advisorGoogleLogin_Usecase(dependencies).executeFunction(email)
     res.status(201).json(response)
