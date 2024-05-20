333fd16895b8471570c1f40edd8d8da0a18b9580
officialrashid <muhammedrashi59@gmail.com>
2024-05-20 13:48:57 +0530

    chat test

--- FRONTEND/Brototype-Frontend/src/routes/AdvisorRoutes.tsx
+++ FRONTEND/Brototype-Frontend/src/routes/AdvisorRoutes.tsx
@@ -10,7 +10,7 @@ import Account from '../components/Advisor/account/Account'
 import ExtPage from '../components/Advisor/extend-request/ExtPage'
 import Student from '../components/Advisor/students/Student'
 import Reviewer from '../components/Advisor/schedule /Reviewer'
-import Chat from "../components/Advisor/Chat/Chat.tsx"
+import Chat from "../components/Advisor/Chat/AdvisorChat"
 import Footer from '../components/Advisor/components/Footer'
 import Review from '../components/Advisor/reviews/Review'
 import { useSelector } from 'react-redux'
