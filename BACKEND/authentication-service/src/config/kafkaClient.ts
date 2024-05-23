fb11cef46b935d0e749d925d24d77dbd18791fdc
officialrashid <muhammedrashi59@gmail.com>
2024-05-23 06:44:58 +0530

    kafka servcie name changed

--- BACKEND/authentication-service/src/config/kafkaClient.ts
+++ BACKEND/authentication-service/src/config/kafkaClient.ts
@@ -2,5 +2,5 @@ import {Kafka} from "kafkajs"
 
 export const kafka = new Kafka({
     clientId: 'authentication',
-    brokers: ['localhost:9092']
+    brokers: ['demo-kafka:9092']
 })
--- BACKEND/reviewer-service/src/config/kafkaClient.ts
+++ BACKEND/reviewer-service/src/config/kafkaClient.ts
@@ -2,5 +2,5 @@ import {Kafka} from "kafkajs"
 
 export const kafka = new Kafka({
     clientId: 'review-book',
-    brokers: ['localhost:9092']
+    brokers: ['demo-kafka:9092']
 })
