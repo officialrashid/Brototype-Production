942e9b948b399026bf12fa4a5ce9811cc93d9c62
rashidmetadrob <rashid@metadrob.com>
2024-08-13 22:12:19 +0530

    check degital occean cluster2

--- FRONTEND/Brototype-Frontend/src/components/Advisor/account/Account.tsx
+++ FRONTEND/Brototype-Frontend/src/components/Advisor/account/Account.tsx
@@ -20,11 +20,12 @@ const Account=()=>{
   <div className="">
     <div className="ml-4 mt-2 mb-0">
     <span className="font-bold text-">Your Details</span>
+    
     </div>
 
  
 
- 
+
  
   <div className="flex ml-4  gap-20">
     <div >
