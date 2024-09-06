011ad3486aa620125561bad2efb382b17ec81d07
sachin-s-c <sachinksiby@gmail.com>
2024-09-06 23:49:41 +0530

    workflow changed

--- BACKEND/advisor-service/src/external-libraries/kafkaService.ts
+++ BACKEND/advisor-service/src/external-libraries/kafkaService.ts
@@ -1,24 +1,20 @@
-import kafkajs,{Partitioners} from 'kafkajs'
+import kafkajs, { Partitioners } from "kafkajs";
 
-const kafkaClient=new kafkajs.Kafka({
-    clientId:'coordinator-service',
-    brokers:['demo-kafka:9092']
-})
+const kafkaClient = new kafkajs.Kafka({
+  clientId: "coordinator-service",
+  brokers: ["demo-kafka:9092"],
+});
 
+const producer = kafkaClient.producer({
+  createPartitioner: Partitioners.LegacyPartitioner,
+});
 
-const producer=kafkaClient.producer({
-    createPartitioner:Partitioners.LegacyPartitioner
-})
+const sendAdvisorData = async (topic: string, message: {}) => {
+  await producer.connect();
+  await producer.send({
+    topic,
+    messages: [{ value: JSON.stringify(message) }],
+  });
+};
 
-const sendAdvisorData=async (topic:string,message:{})=>{
-    await producer.connect()
-    await producer.send({
-        topic,
-        messages:[{value:JSON.stringify(message)}]
-    })
-
-}
-
-
-
-export {sendAdvisorData}
+export { sendAdvisorData };
